d42bf02135f13b9f334b59e9c906726e01dac933
Heba Nageh <>
2021-04-24 04:51:20 +0300

    UI initial files

--- Repos/Views/BaseViewController.swift
+++ Repos/Views/BaseViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  BaseViewController.swift
 //  Repos
 //
 //  Created by Heba Nageh on 23/04/2021.
@@ -7,13 +7,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class BaseViewController: UIViewController {
 
+    var viewTitle: String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
-
 
 }
 
---
+++ Repos/Views/Repository/RepoInfoTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  RepoInfoTableViewCell.swift
+//  Repos
+//
+//  Created by Heba Nageh on 24/04/2021.
+//
+
+import UIKit
+
+class RepoInfoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
